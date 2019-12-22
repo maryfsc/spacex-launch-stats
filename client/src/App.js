213ab301a7781707dca3f9812cc2961e3133b248
Mary Francis <maryfsc@disroot.org>
2019-12-22 17:05:21 -0300

    Adds LaunchItem component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
 import logo from './logo.png'
+import Launches from './components/Launches'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql'
+});
 
 const App = () => (
-  <div className="App">
-    <img src={logo} alt="SpaceX" style={{ width: 300, display: 'block', margin: 'auto' }} />
-  </div>
+  <ApolloProvider client={client}>
+    <div className="container">
+      <img src={logo} alt="SpaceX" style={{ width: 300, display: 'block', margin: 'auto' }} />
+      <Launches />
+    </div>
+  </ApolloProvider>
 )
 
 export default App;
