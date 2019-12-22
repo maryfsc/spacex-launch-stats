497f42541824efbc68bc21a662860aa39251bd7b
Mary Francis <maryfsc@disroot.org>
2019-12-22 16:31:12 -0300

    Fix start script from client

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import logo from './logo.png'
+
+const App = () => (
+  <div className="App">
+    <img src={logo} alt="SpaceX" style={{ width: 300, display: 'block', margin: 'auto' }} />
+  </div>
+)
+
+export default App;
