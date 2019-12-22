ead8318322eb4005bf8693653f52950d4eadce6b
Mary Francis <maryfsc@disroot.org>
2019-12-22 18:19:54 -0300

    Sets up public folder

---
+++ public/precache-manifest.8637e32c66ef9bcef7181337f5d3e61a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65422a7b09f264548e7cf5e9440cf067",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d40e4a26f52d0568cecc",
+    "url": "/static/js/2.b10fc354.chunk.js"
+  },
+  {
+    "revision": "b95ac56ec0dfc8a73bc74705cd57dbe0",
+    "url": "/static/js/2.b10fc354.chunk.js.LICENSE"
+  },
+  {
+    "revision": "91aa537b75d15f9bbef7",
+    "url": "/static/js/main.679b0704.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  },
+  {
+    "revision": "2bfc8e40f3b12c3780bbfc7494274cfd",
+    "url": "/static/media/logo.2bfc8e40.png"
+  }
+]);
